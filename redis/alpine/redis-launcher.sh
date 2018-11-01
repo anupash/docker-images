eccbffd1844fa7cbf9c9ee95afa92bb6f62b9aea
Anupam Ashish <a.ashish@yieldlab.de>
2018-11-01 16:20:50 +0100

    Fix maxmemory computation suffers from integer overflow

--- redis/alpine/redis-launcher.sh
+++ redis/alpine/redis-launcher.sh
@@ -48,6 +48,11 @@ MASTER_LB_PORT="${!PORTVAR}"
 MASTER_LB_HOST="${!HOSTVAR}"
 QUORUM=${QUORUM:-2}
 
+# Configure maxmemory based on cgroup limits.
+# First, find the own memory cgroup by iterating over all cgroups.
+MEMORY_LIMIT_BY_CGROUP=$(cat $(dirname $(find /sys/fs/cgroup/memory/ -name "tasks"|xargs grep -l ^1$))/memory.limit_in_bytes)
+MEMORY_LIMIT=$(( $MEMORY_LIMIT_BY_CGROUP / 100 * 98 )) # 98% of allowed memory
+
 # Only sets AUTH if the ENV var REDIS_PASS is set.
 REDISAUTH=""
 [ -n "$REDIS_PASS" ] && REDISAUTH="-a $REDIS_PASS" || REDISAUTH=""
@@ -66,10 +71,6 @@ function launchmaster() {
     sed -i "s/# requirepass/requirepass ${REDIS_PASS} \n#/" $MASTER_CONF
   fi
 
-  # Configure maxmemory based on cgroup limits.
-  # First, find the own memory cgroup by iterating over all cgroups.
-  MEMORY_LIMIT_BY_CGROUP=$(cat $(dirname $(find /sys/fs/cgroup/memory/ -name "tasks"|xargs grep -l ^1$))/memory.limit_in_bytes)
-  MEMORY_LIMIT=$(( $MEMORY_LIMIT_BY_CGROUP * 98 /100 )) # 98% of allowed memory
   sed -i "s/# maxmemory .*/maxmemory $MEMORY_LIMIT/" $MASTER_CONF
 
   redis-server $MASTER_CONF --protected-mode no $@
@@ -147,11 +148,6 @@ function launchslave() {
 
   sed -i "s/%master-ip%/${MASTER_LB_HOST}/" $SLAVE_CONF
   sed -i "s/%master-port%/${MASTER_LB_PORT}/" $SLAVE_CONF
-
-  # Configure maxmemory based on cgroup limits.
-  # First, find the own memory cgroup by iterating over all cgroups.
-  MEMORY_LIMIT_BY_CGROUP=$(cat $(dirname $(find /sys/fs/cgroup/memory/ -name "tasks"|xargs grep -l ^1$))/memory.limit_in_bytes)
-  MEMORY_LIMIT=$(( $MEMORY_LIMIT_BY_CGROUP * 98 /100 )) # 98% of allowed memory
   sed -i "s/# maxmemory .*/maxmemory $MEMORY_LIMIT/" $SLAVE_CONF
 
   redis-server $SLAVE_CONF --protected-mode no $@
